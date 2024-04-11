4a57862a92a330bd8c2082e00397b6a33e91e61a
hustlehoff-dev <148590992+hustlehoff-dev@users.noreply.github.com>
2024-04-12 01:37:18 +0200

    Strapi implement

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -3,35 +3,73 @@ import './Cart.css'
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 
 const Cart = () => {
+  const data = [
+    {
+      id: 1,
+      img: "../../../img/woman_1.webp",
+      img2: "../../../img/woman_1_1.webp",
+      title: "Product Mockup",
+      isNew: true,
+      oldPrice: 249,
+      price: 99,
+    },
+    {
+      id: 2,
+      img: "../../../img/woman_8.webp",
+      img2: "../../../img/woman_8_1.webp",
+      title: "Product Mockup",
+      isNew: true,
+      oldPrice: 249,
+      price: 199,
+    },
+    {
+      id: 3,
+      img: "../../../img/woman_3.webp",
+      img2: "../../../img/woman_3_1.webp",
+      title: "Product Mockup",
+      isNew: true,
+      oldPrice: 249,
+      price: 299,
+    },
+    {
+      id: 4,
+      img: "../../../img/woman_7.webp",
+      img2: "../../../img/woman_7_1.webp",
+      title: "Product Mockup",
+      isNew: true,
+      oldPrice: 249,
+      price: 199,
+    },
+  ];
+
+  const totalPrice = () => {
+    let total = 0;
+    data.forEach((item)=> {
+      total += item.quantity * item.price;
+    });
+    return total.toFixed(2);
+  };
+
   return (
     <div className='cart'>
         <h3>Your lovely cart</h3>
-        <div className="cartItem">
-            <div className="cartItemImage">
-                <img src="../../../img/woman_1.webp" alt="Cart product image" />
-            </div>
-            <div className="cartItemContent">
-                <span>Big Booty Leggins</span>
-                <span className='cartItemAmount'>1 x $199</span>
-            </div>
-            <div className="cartItemRemove">
-            <DeleteOutlineIcon/>
-            </div>
-        </div>
-        <div className="cartItem">
-            <div className="cartItemImage">
-                <img src="../../../img/woman_1.webp" alt="Cart product image" />
-            </div>
-            <div className="cartItemContent">
-                <span>Big Booty Leggins</span>
-                <span className='cartItemAmount'>1 x $199</span>
-            </div>
-            <div className="cartItemRemove">
-            <DeleteOutlineIcon/>
-            </div>
-        </div>
+        {data.map((item)=>(
+          <div className="cartItem" key={item.id}>
+          <div className="cartItemImage">
+              <img src={item.img} alt="Cart product image" />
+          </div>
+          <div className="cartItemContent">
+              <span>{item.title}</span>
+              <span className='cartItemAmount'>{item.quantity} x ${item.price}</span>
+          </div>
+          <div className="cartItemRemove">
+          <DeleteOutlineIcon/>
+          </div>
+      </div>
+        ))}
         
-        <span>Subtotal <span>$199</span></span>
+        
+        <span>Subtotal <span>${totalPrice()}</span></span>
         <button>Checkout</button>
     </div>
   )
